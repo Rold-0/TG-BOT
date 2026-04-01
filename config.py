2f7980c8c875c911da5cf2150e640a7ad12704fe
Rold-0 <dorll2893@gmail.com>
2026-04-01 13:21:14 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = '8752647787:AAHPPS6Prsm4-yx8c3MkpzNHuS29OvaRo24'
+API_KEY = 'Rb298X4A1B0FuiGryOl6sYc3bkftDO6TF5nodUPK'
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
